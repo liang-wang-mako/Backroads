4abe31ce51e9aa40846177419a453cc9442c9d97
Liang Wang <lwang@micro-wang.com>
2023-07-22 00:00:59 +1200

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import About from "./components/About"
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import Services from "./components/Services"
-import Tours from "./components/Tours"
+import About from './components/About'
+
+import Footer from './components/Footer'
+
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+import Tours from './components/Tours'
 
 function App() {
   console.log('Hello')
   return (
-  <>
-  <Navbar />
-  <Hero />
-  <About />
-  <Services />
-  <Tours />
-  <Footer />
-  </>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
   )
 }
 
